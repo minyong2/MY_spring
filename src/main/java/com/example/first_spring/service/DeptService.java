620de7a1d1a92095c6097172a6ab64c1bedbaa83
ᴍɪɴᴏᴏɴɢ <owow77@naver.com>
2022-05-04 15:51:26 +0900

    feat : Dept의 모든 데이터를 조회하는 코드

---
+++ src/main/java/com/example/first_spring/service/DeptService.java
@@ -0,0 +1,22 @@
+package com.example.first_spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.first_spring.mapper.DeptMapper;
+import com.example.first_spring.vo.DeptVO;
+
+@Service
+public class DeptService {
+	
+	@Autowired
+	private DeptMapper deptMapper;
+	
+	
+	public List<DeptVO> getDeptList(){
+		return deptMapper.getDeptList();
+	}
+
+}
---
+++ src/main/java/com/example/first_spring/vo/DeptVO.java
@@ -0,0 +1,11 @@
+package com.example.first_spring.vo;
+
+import lombok.Data;
+
+@Data
+public class DeptVO {
+	private int DEPTNO;
+	private String DNAME;
+	private String LOC;
+
+}
